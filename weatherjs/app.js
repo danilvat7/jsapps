7638de4f0809610c211689f06cc63dea6b35dad3
Daniyil <danilvat7@gmail.com>
2018-03-27 00:20:56 +0300

    deploy weatherjs

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,15 +1,37 @@
 /*jshint esversion: 6 */
-// init weather obj
-const weather = new Weather("Boston", "MA");
 
+// init storage
+const storage = new Storage();
+// get storade location data
+const weatherLocation = storage.getLocationData();
+// init weather obj
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+// init ui
+
+const ui = new UI();
 // get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// chage location event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city, state);
+
+  // set location in LS
+  storage.setLocationData(city, state);
+  // get weather again
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 function getWeather() {
   weather
     .getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
